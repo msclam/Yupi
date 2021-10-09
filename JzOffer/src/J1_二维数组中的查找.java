ca71a5a527a4fcc172f98a9ca1bc73f0b2f0770f
koiman <1938181753@qq.com>
2021-10-09 20:42:17 +0800

    commit all

---
+++ JzOffer/src/J1_二维数组中的查找.java
@@ -0,0 +1,19 @@
+public class J1_二维数组中的查找 {
+    public static boolean Find(int target, int[][] A) {
+        if (A.length == 0 || A[0].length == 0) return false;
+        int m = A.length, n = A[0].length;
+        int i = 0, j = n - 1;
+        while (i < m && j >= 0) {
+            if (A[i][j] == target) return true;
+            else if (A[i][j] > target) j--;
+            else i++;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int target = 7;
+        int[][] A = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+        System.out.println(Find(target, A));
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Yupi
+关于项目的查找
+
+in:name springboot stars:>4000 pushed:>2020-01-01 language:java forks:>1000
+
+in:readme:spider 淘宝 stars:>1000 pushed:>2020-09-01
+
+
+
+![image-20211009173016945](C:\Users\tesseract\AppData\Roaming\Typora\typora-user-images\image-20211009173016945.png)
